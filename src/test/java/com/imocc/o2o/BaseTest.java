41cb8fde58f47c8556e034f172829a67d643000c
kayang0000 <18410422493@163.com>
2019-02-15 14:02:31 +0800

    junit test AreaService.java

--- src/test/java/com/imocc/o2o/BaseTest.java
+++ src/test/java/com/imocc/o2o/BaseTest.java
@@ -13,6 +13,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @date 2019/2/15 13:27
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml"})
 public class BaseTest {
 }
