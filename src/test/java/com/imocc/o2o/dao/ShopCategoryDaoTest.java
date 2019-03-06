563e65836ec4c5e242cdeff8fee6da5818fd9073
kayang0000 <18410422493@163.com>
2019-03-06 14:05:37 +0800

    add shopCategoryDao file and testjunit

---
+++ src/test/java/com/imocc/o2o/dao/ShopCategoryDaoTest.java
@@ -0,0 +1,28 @@
+package com.imocc.o2o.dao;
+
+import com.imocc.o2o.BaseTest;
+import com.imooc.o2o.dao.ShopCategoryDao;
+import com.imooc.o2o.entity.ShopCategory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ShopCategoryDaoTest extends BaseTest {
+    @Autowired
+    private ShopCategoryDao shopCategoryDao;
+
+    @Test
+    public void testQueryShopCategory(){
+        List<ShopCategory> shopCategoryList = shopCategoryDao.queryShopCategory(new ShopCategory());
+        Assert.assertEquals(2, shopCategoryList.size());
+
+        ShopCategory shopCategory = new ShopCategory();
+        ShopCategory shopCategoryParent = new ShopCategory();
+        shopCategoryParent.setShopCategoryId(1L);
+        shopCategory.setParent(shopCategoryParent);
+        shopCategoryList = shopCategoryDao.queryShopCategory(shopCategory);
+        Assert.assertEquals(1, shopCategoryList.size());
+    }
+}
