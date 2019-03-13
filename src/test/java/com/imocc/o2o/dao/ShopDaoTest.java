2e7a7faa85efd577310baea6fb6c1a3ee76385c6
kayang0000 <18410422493@163.com>
2019-03-13 14:07:31 +0800

    **Database master/slave replication and read/write separation

--- src/test/java/com/imocc/o2o/dao/ShopDaoTest.java
+++ src/test/java/com/imocc/o2o/dao/ShopDaoTest.java
@@ -19,7 +19,6 @@ public class ShopDaoTest extends BaseTest {
     private ShopDao shopDao;
 
     @Test
-    @Ignore
     public void testInsertShop() {
         Shop shop = new Shop();
         PersonInfo owner = new PersonInfo();
@@ -45,6 +44,7 @@ public class ShopDaoTest extends BaseTest {
     }
 
     @Test
+    @Ignore
     public void testUpdateShop() {
         Shop shop = new Shop();
         shop.setShopId(1L);
